8c1e8b7801377cb2b617e649db7324ff603684fb
Harsh Gupta <harsh3101gupta@gmail.com>
2022-06-05 04:33:54 +0530

    first commit

--- lib/screens/Housing.dart
+++ lib/screens/Housing.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Housing extends StatelessWidget {
-  const Housing({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
